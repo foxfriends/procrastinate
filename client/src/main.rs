f2260d4ad52a034ed32bd865bc706e17c2bbac57
Cameron Eldridge <cameldridge@gmail.com>
2022-07-07 22:26:16 -0400

    functional components

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,27 +1,13 @@
 use yew::prelude::*;
 
-struct App;
-
-impl Component for App {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        false
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <div class="flex flex-col min-h-screen">
-                <div class="grow">
-                    {"Hello world"}
-                </div>
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <div class="flex flex-col min-h-screen">
+            <div class="grow">
+                {"Hello world"}
             </div>
-        }
+        </div>
     }
 }
 
