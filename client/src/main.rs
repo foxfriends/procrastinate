584e884f3377ed3812d7b522627368b9b7ba8318
Cameron Eldridge <cameldridge@gmail.com>
2022-07-07 22:16:03 -0400

    Set up tailwind

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,44 +1,30 @@
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-}
+struct App;
 
-struct Model {
-    value: i64,
-}
-
-impl Component for Model {
-    type Message = Msg;
+impl Component for App {
+    type Message = ();
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0 }
+        Self
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
+        false
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
+            <div class="flex flex-col min-h-screen">
+                <div class="grow">
+                    {"Hello world"}
+                </div>
             </div>
         }
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<App>();
 }
